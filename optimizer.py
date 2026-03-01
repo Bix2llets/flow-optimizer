69bff0fbb1c64b8431b8c7c41367d2a4c73ad14e
Nguyen Hoang Thuan Phat <thuanphat4b@gmail.com>
2026-03-01 22:08:16 +0700

    Add the constraints of bandwidth and previous output to layer optimization

--- optimizer.py
+++ optimizer.py
@@ -1,5 +1,6 @@
 import cvxpy as cp
 import numpy as np
+from network import Node, Link
 
 
 def cvxpy_testing_function():
@@ -21,3 +22,35 @@ def cvxpy_testing_function():
 
 
 cvxpy_testing_function()
+
+
+def optimize_layer(
+    current_layer: list[Node], prev_layer: list[Node], connecting_edge: list[Link]
+):
+    constraints = []
+    edge_value = cp.Variable(len(connecting_edge))
+    # NOTE: Bound the flow between 0 and bandwidth (both inclusive)
+    edge_cap = np.array([link.flow.cap_value for link in connecting_edge])
+
+    constraints.append(0 <= edge_value)
+    constraints.append(edge_value <= edge_cap)
+
+    # NOTE: Add the constraint of matching previous layer's output
+    compress_map = {}
+    for node in prev_layer:
+        if node.id in compress_map:
+            pass
+        else:
+            compress_map[node.id] = len(compress_map)
+
+    prev_output_vector = [node.output.actual_value for node in prev_layer]
+    output_mul_matrix = np.zeros((len(prev_layer), len(connecting_edge)))
+    for node in prev_layer:
+        row = compress_map[node]
+        for col, edge in enumerate(connecting_edge):
+            if edge.start_id == node.id:
+                output_mul_matrix[row, col] = 1
+
+    constraints.append(output_mul_matrix @ edge_value == prev_output_vector)
+
+    pass
